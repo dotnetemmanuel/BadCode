82b88f09d32aa82e341827f177d5aa7823d09849
Emmanuel Duchene <emmanuel.duchene@gmail.com>
2024-11-13 09:48:06 +0100

    first commit

---
+++ InvoiceSender.Test/InvoiceHelperTests.cs
@@ -0,0 +1,140 @@
+namespace InvoiceSender.Test;
+using System.Reflection;
+
+public class InvoiceHelperTests
+{
+    // TODO: Add test case for user with 2 invoices
+    [Theory]
+    [InlineData("daniel@mindfulstack.se", 1)]
+    [InlineData("ruben@mindfulstack.se", 0)]
+    public void SendsCorrectNumberOfEmails(string email, int expected)
+    {
+        var db = new TestDatabase();
+        var emailSender = new TestEmailSender();
+
+        var sut = new InvoiceHelper(db, emailSender);
+        // Act
+        var actual = sut.Run(email);
+
+        // Assert
+        Assert.Equal(expected, actual.Count);
+    }
+    
+    // TODO: Ajust for multiple messages
+    [Fact]
+    public void SendsCorrectMessage()
+    {
+        // Arrange
+        var db = new TestDatabase();
+        var emailSender = new TestEmailSender();
+        var expected = "Dear Daniel,\n\nYour invoice of 10 is " +
+                       "due in 10 days.\n\nBest regards,\nThe Invoice Team";
+
+        var sut = new InvoiceHelper(db, emailSender);
+        // Act
+        var actual = sut.Run("daniel@mindfulstack.se");
+
+        // Assert
+        Assert.Equal(expected, actual[0]);
+    }
+
+    [Fact]
+    public void CalculatesDueDateProperly()
+    {
+        // TODO: Test different due dates (especially around 0)
+    }
+    
+    [Fact]
+    public void PrivateMethodTest()
+    {
+        // Arrange
+        var db = new Database();
+        var emailSender = new EmailSender();
+        
+        var helper = new InvoiceHelper(db, emailSender);
+        
+        var expected = "Dear Daniel,\n\nYour invoice of 10 is " +
+                       "due in 10 days.\n\nBest regards,\nThe Invoice Team";
+        // Act
+        var actual = helper.TestGetInvoice("Daniel", 10, 10);
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+    
+    [Fact]
+    public void PrivateMethodTestWithReflection()
+    {
+        // Arrange
+        var db = new Database();
+        var emailSender = new EmailSender();
+        
+        var helper = new InvoiceHelper(db, emailSender);
+        
+        var expected = "Dear Daniel,\n\nYour invoice of 10 is " +
+                       "due in 10 days.\n\nBest regards,\nThe Invoice Team";
+        var args = new object[] { "Daniel", 10m, 10 };
+        
+        // Act
+        var privat = typeof(InvoiceHelper).GetMethod("GetInvoice", BindingFlags.NonPublic | BindingFlags.Instance);
+        var actual = privat.Invoke(helper, args);
+        
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+}
+
+internal class TestDatabase : IDatabase
+{
+    public User GetUser(string email)
+    {
+        switch (email)
+        {
+            case "daniel@mindfulstack.se":
+                return new User
+                {
+                    Id = 1,
+                    Name = "Daniel",
+                    Email = email
+                };
+            case "ruben@mindfulstack.se":
+                return new User
+                {
+                    Id = 2,
+                    Name = "Ruben",
+                    Email = email
+                };
+            default:
+                return new User
+                {
+                    Id = 0,
+                    Name = "Unknown",
+                    Email = email
+                };
+        }
+    }
+
+    public Invoice? GetInvoice(int userId)
+    {
+        switch (userId)
+        {
+            case 1:
+                return new Invoice
+                {
+                    Amount = 10,
+                    DueDate = DateTime.Now.AddDays(10)
+                };
+            default:
+                return null;
+        }
+
+    }
+}
+
+internal class TestEmailSender : IEmailSender
+{
+    public void SendEmail(string email, string subject, string messsage)
+    {
+        // Do nothing
+    }
+}
---
+++ InvoiceSender/Program.cs
@@ -0,0 +1,80 @@
+﻿// This is NOT how you should be coding!
+
+namespace InvoiceSender;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        var db = new Database();
+        var emailSender = new EmailSender();
+        
+        var helper = new InvoiceHelper(db, emailSender);
+
+        var email = "john@example.com";
+        
+        // Should not be allowed!
+        //var s = helper.GetInvoice("John", 100, 10);
+        
+        var invoicesSent = helper.Run(email);
+        
+        Console.WriteLine($"Sent {invoicesSent} invoices to {email}");
+
+    }
+}
+
+public class InvoiceHelper
+{
+    private readonly IDatabase _db;
+    private readonly IEmailSender _emailSender;
+
+    public InvoiceHelper(IDatabase db, IEmailSender emailSender)
+    {
+        _db = db;
+        _emailSender = emailSender;
+    }
+
+    public List<string> Run(string email)
+    {
+        var messages = new List<string>();
+        var user = _db.GetUser(email);
+        // TODO: Enable several invoices per user
+        // Could this be more than one?
+        var invoice = _db.GetInvoice(user.Id);
+        if (invoice is null)
+            return messages;
+        
+        var dueDate = invoice.DueDate;
+        var daysToDueDate = (dueDate.Date - DateTime.Now.Date).Days;
+
+        var message = GetInvoice(user.Name, invoice.Amount, daysToDueDate);
+        SendInvoice(message, user.Email);
+        messages.Add(message);
+        
+        return messages;
+    }
+    
+    private string GetInvoice(string name, decimal amount, int daysToDueDate)
+    {
+        // Note: This method has tests using reflection, but it's not the best way to test it
+        // Update tests if you change this method
+        return $"Dear {name},\n\nYour invoice of {amount} is " +
+               $"due in {daysToDueDate} days.\n\nBest regards,\nThe Invoice Team";
+    }
+
+    private void SendInvoice(string message, string email)
+    {
+        _emailSender.SendEmail(email, "Invoice Reminder", message);
+    }
+    
+    // TODO: how do we test this logic through the Run method?
+    internal string TestGetInvoice(string name, decimal amount, int daysToDueDate)
+    {
+        if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
+        {
+            throw new InvalidOperationException("This method should not be called in production!");
+        }
+        
+        return GetInvoice(name, amount, daysToDueDate);
+    }
+}
